239f5f3cb8a2127991347c9438444426b0a3b10c
Kaloyan Karagyozov <karagyozovkaloyan@gmail.com>
2022-10-24 16:12:06 +0300

    small style changes

--- change-theme.js
+++ change-theme.js
@@ -12,14 +12,13 @@ function changeTheme(e) {
 
     } else if (e.target.textContent == `Light theme`) {
         e.target.textContent = `Dark theme`;
-        e.target.parentElement.style.backgroundColor = `#c7ccd0`;
+        e.target.parentElement.style.backgroundColor = `#F0F2F5`;
         e.target.parentElement.style.color = `black`;
         e.target.parentElement.parentElement.style.backgroundColor = `#c7ccd0`;
         e.target.parentElement.parentElement.style.color = `black`;
         resultsDiv.style.backgroundColor = '#c7ccd0'
         resultsDiv.style.color = 'black';
         e.target.style.border = '1px solid black'
-
     }
 }
 export default changeTheme;
